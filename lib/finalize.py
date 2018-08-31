4f2fa237b177d49323281f0442c1073aad2e9193
Sterling Baldwin <baldwin32@llnl.gov>
2018-08-30 17:18:28 -0700

    Adds calls to generate filestructure and move files for esgf publication

--- lib/finalize.py
+++ lib/finalize.py
@@ -6,7 +6,7 @@ from shutil import rmtree
 from lib.jobstatus import JobStatus
 from lib.mailer import Mailer
 from lib.util import print_message, print_line, print_debug
-
+from lib.esgfutils import structure_gen, move_or_copy, mapfile_gen
 
 def finalize(config, event_list, status, runmanager):
     if status == 1 and config['global'].get('native_grid_cleanup') in [1, '1', 'true', 'True']:
@@ -15,6 +15,41 @@ def finalize(config, event_list, status, runmanager):
     else:
         message = 'Leaving native grid files in place'
     print_message(message, 'ok')
+
+    esgf = config.get('esgf')
+    if esgf:
+        message = 'Starting file structure generation for ESGF publication'
+        print_message(message, 'ok')
+        grids = list()
+        pp = config.get('post-processing')
+        if pp:
+            if pp.get('climo'):
+                grids.append(pp['climo']['destination_grid_name'])
+            if pp.get('timeseries'):
+                if pp['timeseries']['destination_grid_name'] not in grids:
+                    grids.append(pp['timeseries']['destination_grid_name'])
+            if pp.get('regrid'):
+                for datatype in ['atm', 'lnd']:
+                    if pp['regrid'].get(datatype):
+                        if pp['regrid'][datatype]['destination_grid_name'] not in grids:
+                            grids.append(pp['regrid'][datatype]['destination_grid_name'])
+        for case in runmanager.cases:
+            case_name = config['simulations'][case['case']]['short_name']
+            message = 'Setting up structure for {}'.format(case_name)
+            print_message(message, 'ok')
+            structure_gen(
+                basepath=esgf['esgf_publication_path'],
+                casename=case_name,
+                grids=grids,
+                atmos_res=esgf['atmos_res_name'],
+                ocean_res=esgf['ocean_res_name'])
+            message = 'Moving data into place'
+            print_message(message, 'ok')
+            move_or_copy(
+                basepath=esgf['esgf_publication_path'],
+                config=config,
+                mode=esgf['move_or_copy'],
+                case=case['case'])
 
 
     if status == 1:
@@ -30,7 +65,7 @@ def finalize(config, event_list, status, runmanager):
     print_message(msg, code)
     emailaddr = config['global'].get('email')
     if emailaddr:
-        message='Sending notification email to {}'.format(emailaddr)
+        message = 'Sending notification email to {}'.format(emailaddr)
         print_message(message, 'ok')
         try:
             if status == 1:
@@ -55,6 +90,7 @@ def finalize(config, event_list, status, runmanager):
                 msg=msg)
         except Exception as e:
             print_debug(e)
+
 
     logging.info("All processes complete")
 
